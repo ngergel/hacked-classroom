746d5b132d921327a553938cc025922a5745d1c1
sichunx10 <sichunx10@users.noreply.github.com>
2019-01-20 08:21:26 -0700

    Fixed Spacecut for good

--- SpaceCut.py
+++ SpaceCut.py
@@ -1,13 +1,36 @@
 def URLfromlist(raw):
+    #take each element from raw course list
     for course in raw:
+        #finds the location of the first number and cuts everything before
         for char in course:
             if char.isdigit():
                 cutlocation = char
                 break
         coursename = course[0:int(cutlocation)]
         newstring = course[int(cutlocation)+1:]
-        
-        return (coursename, newstring)
+        #finds the next space of new string and cuts out the course number
+        for i in range(len(newstring)):
+            if newstring[i] == " ":
+                cutlocation2 = i
+                break
+        coursenum = newstring[0:cutlocation2]
+
+        #replaces spaces with %20 (this is how the url is organized)
+        coursename.replace(" ", "%20")
+
+        #creates a url based on course name and course number
+        url = str("https://catalogue.ualberta.ca/Course/Details?subjectCode=" + coursename + "&catalog=" + coursenum)
+        print(url)
+
+
+'''
+    for i in range(len(string)):
+        if char in string == " ":
+            cutlocation = i
+            print(cutlocation)
+            break
+'''
+
 
 list = ["ACCTG 501 - Introduction to Financial Reporting and Analysis", "ACCTG 503 - Introduction to Financial Reporting and Analysis"]
 
