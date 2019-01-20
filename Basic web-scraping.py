7d7cc7c3a602a379845a0c61958184bea72c8b35
sichunx10 <sichunx10@users.noreply.github.com>
2019-01-19 20:14:50 -0700

    Edit 1
    split the string into day of week, start/end time, and room

--- Basic web-scraping.py
+++ Basic web-scraping.py
@@ -21,3 +21,40 @@ containers = page_soup.findAll("div", {"class": "claptrap-class"})
 
 for container in containers:
     print(container.em)
+
+
+#we're just looking at the first container
+container = str(containers[0].em)
+
+#looking for the first space in the container
+def findSpace(containername):
+    for i in range(len(container)):
+        if containername[i] == " ":
+            return i
+
+#stores the day of week the class occurs in
+index = findSpace(container)
+dayOfWeek = container[4:index]
+
+#make the new string (should be shorter)
+container2 = container[index+1:]
+
+#isolating starttime
+index2 = findSpace(container2)
+startTime = container2[0:index2]
+container3 = container2[index2:]
+
+print("Day of week is: " + dayOfWeek)
+print("start time is: " + startTime)
+
+#isolating endtime
+endtime = container3[2:11]
+print("end time is: " + endtime)
+
+room = container3[12:-5]
+print("room is located: " + room)
+
+
+
+
+
